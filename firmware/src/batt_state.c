9b3b31834f6d12e9027953075b943bac9dbb2e89
Prithvi <p@earthlord.io>
2025-01-29 20:20:54 +0530

    Batt state enables use of ADC offset

--- firmware/src/batt_state.c
+++ firmware/src/batt_state.c
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2025, Cranberry Fit (Empower Digital Health Pvt Ltd)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "batt_state.h"
 #include "ti/driverlib/m0p/dl_core.h"
 #include "ti_msp_dl_config.h"
@@ -27,13 +55,14 @@ batt_state_t batt_state_get(void)
     int16_t batt_value, usb_vin_value;
 
     /* Get calibrated ADC offset - workaround for ADC_ERR_06 */
-    int16_t ADC_offset = 0;
-    //     DL_ADC12_getADCOffsetCalibration(ADC12_0_ADCMEM_VBAT_REF_VOLTAGE_V);
+    int16_t ADC_offset = 
+        DL_ADC12_getADCOffsetCalibration(ADC12_0_ADCMEM_VBAT_REF_VOLTAGE_V);
 
     DL_ADC12_startConversion(ADC12_0_INST);
     
     delay_cycles(500*DELAY_1_US_COUNT);
     
+    //This wasn't working, not sure why
 #if 0
     while(ADC12_STATUS_BUSY_ACTIVE == 
             (DL_ADC12_getStatus(ADC12_0_INST) & ADC12_STATUS_BUSY_MASK)){}
