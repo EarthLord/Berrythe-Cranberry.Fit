2ac9ee6645a1ca96245d439ef113e6803d7bc14f
Prithvi <p@earthlord.io>
2025-01-29 20:22:55 +0530

    Breathe mode uses another sequence for blue exhale
    and uses 16 bit with gamma correction to reduce blockiness in fade in or out

--- firmware/src/breathe_mode.c
+++ firmware/src/breathe_mode.c
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2025, Cranberry Fit (Empower Digital Health Pvt Ltd)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "breathe_mode.h"
 #include "off_mode.h"
 #include "periodic_timer.h"
@@ -9,16 +37,20 @@
 #include "low_batt_mode.h"
 #include "night_mode.h"
 #include "lp5810.h"
+#include "math.h"
+#include <limits.h>
 
 #define BREATHE_MODE_TIMEOUT_PERIOD_MIN     (10)
 #define BREATHE_MODE_TIMEOUT_PERIOD_MS      (BREATHE_MODE_TIMEOUT_PERIOD_MIN*60*1000)
-//#define BREATHE_MODE_TIMEOUT_COUNT          (BREATHE_MODE_TIMEOUT_PERIOD_MS/PERIODIC_TIMER_PERIOD_MS)
-
-#define BREATHE_MODE_TIMEOUT_COUNT          (0x80000000)
+#define BREATHE_MODE_TIMEOUT_COUNT          (BREATHE_MODE_TIMEOUT_PERIOD_MS/PERIODIC_TIMER_PERIOD_MS)
 
 #define BREATHE_MODE_COUNT_PERIOD_MS(x)     (x/PERIODIC_TIMER_PERIOD_MS)
 
-#define PERCENT_TO_HEX(x)                   ((x*255)/100)
+#define GAMMA                               (2.2)
+
+#define PERCENT_TO_I16(x)                   ((x*INT16_MAX)/100)
+
+#define ROUNDING_DIVISION_POSITIVE(n,d)     ((n+(d/2))/d)
 
 typedef enum
 {
@@ -34,38 +66,59 @@ typedef enum
     GREEN_TO_PURPLE,
     PURPLE_STEADY,
     PURPLE_TO_BLUE,
+    BLUE_SHARP_FALL,
     BLUE_FALL,
     BLUE_GREEN,
     BREATHE_SEQ_MAX
 }breathe_seq_t;
 
-const static uint16_t breathe_seq_len_count[BREATHE_SEQ_MAX] = 
+const static uint16_t breathe_seq_len_count[BREATHE_SEQ_MAX+1] = 
 {
     BREATHE_MODE_COUNT_PERIOD_MS(4000),       //GREEN_RISE,
     BREATHE_MODE_COUNT_PERIOD_MS(20),         //GREEN_TO_PURPLE,
     BREATHE_MODE_COUNT_PERIOD_MS(6900),       //PURPLE_STEADY,
     BREATHE_MODE_COUNT_PERIOD_MS(200),        //PURPLE_TO_BLUE,
-    BREATHE_MODE_COUNT_PERIOD_MS(7900),       //BLUE_FALL,
+    BREATHE_MODE_COUNT_PERIOD_MS(1000),       //BLUE_SHARP_FALL,
+    BREATHE_MODE_COUNT_PERIOD_MS(6900),       //BLUE_FALL,
     BREATHE_MODE_COUNT_PERIOD_MS(20),         //BLUE_GREEN,
+    0   //BREATHE_SEQ_MAX for 1st time logic to work
 };
 
-const static uint8_t breathe_pwm_value[BREATHE_SEQ_MAX+1][COLOR_MAX] = 
+const static uint16_t breathe_pwm_percent[BREATHE_SEQ_MAX+1][COLOR_MAX] = 
 {
-    //Blue              Green               Red
-    {PERCENT_TO_HEX(000),PERCENT_TO_HEX(025),PERCENT_TO_HEX(000)},  //GREEN_RISE,
-    {PERCENT_TO_HEX(000),PERCENT_TO_HEX(100),PERCENT_TO_HEX(000)},  //GREEN_TO_PURPLE,
-    {PERCENT_TO_HEX(100),PERCENT_TO_HEX(060),PERCENT_TO_HEX(80)},  //PURPLE_STEADY,
-    {PERCENT_TO_HEX(100),PERCENT_TO_HEX(060),PERCENT_TO_HEX(80)},  //PURPLE_TO_BLUE,
-    {PERCENT_TO_HEX(100),PERCENT_TO_HEX(000),PERCENT_TO_HEX(000)},  //BLUE_FALL,
-    {PERCENT_TO_HEX(070),PERCENT_TO_HEX(000),PERCENT_TO_HEX(000)},  //BLUE_GREEN,
-    {PERCENT_TO_HEX(000),PERCENT_TO_HEX(025),PERCENT_TO_HEX(000)},  //Goes back,
+    //Blue,             Green,              Red
+    {PERCENT_TO_I16(  0),PERCENT_TO_I16( 10),PERCENT_TO_I16(  0)},  //GREEN_RISE,
+    {PERCENT_TO_I16(  0),PERCENT_TO_I16( 80),PERCENT_TO_I16(  0)},  //GREEN_TO_PURPLE,
+    {PERCENT_TO_I16(100),PERCENT_TO_I16( 60),PERCENT_TO_I16( 80)},  //PURPLE_STEADY,
+    {PERCENT_TO_I16(100),PERCENT_TO_I16( 60),PERCENT_TO_I16( 80)},  //PURPLE_TO_BLUE,
+    {PERCENT_TO_I16(100),PERCENT_TO_I16(  0),PERCENT_TO_I16(  0)},  //BLUE_SHARP_FALL,
+    {PERCENT_TO_I16( 70),PERCENT_TO_I16(  0),PERCENT_TO_I16(  0)},  //BLUE_FALL,
+    {PERCENT_TO_I16( 10),PERCENT_TO_I16(  0),PERCENT_TO_I16(  0)},  //BLUE_GREEN,
+    {PERCENT_TO_I16(  0),PERCENT_TO_I16( 25),PERCENT_TO_I16(  0)},  //Goes back,
 };
+
+uint16_t gamma2_correction_i16(uint16_t value)
+{
+    //Optimized for Gamma value of 2 from
+    //https://learn.adafruit.com/led-tricks-gamma-correction/the-longer-fix
+    uint16_t result;
+    uint32_t max = (1<<15);   
+    uint32_t value_squared = ((uint32_t)value*value);
+    result = ROUNDING_DIVISION_POSITIVE(value_squared,max);
+    return (result);
+}
 
 void breathe_compute_set_seq_led_value(void)
 {
     static breathe_seq_t breathe_seq = GREEN_RISE;
     static uint16_t seq_count = 0;
     uint8_t colors[COLOR_MAX];
+    static int16_t diff[COLOR_MAX] = 
+    {
+        (breathe_pwm_percent[GREEN_TO_PURPLE][BLUE] - breathe_pwm_percent[GREEN_RISE][BLUE]),
+        (breathe_pwm_percent[GREEN_TO_PURPLE][GREEN] - breathe_pwm_percent[GREEN_RISE][GREEN]),
+        (breathe_pwm_percent[GREEN_TO_PURPLE][RED] - breathe_pwm_percent[GREEN_RISE][RED]),
+    };
 
     if(breathe_seq_len_count[breathe_seq] == seq_count)
     {
@@ -75,6 +128,22 @@ void breathe_compute_set_seq_led_value(void)
         {
             breathe_seq = GREEN_RISE;
         }//No else
+
+        //Difference doesn't change for the sequence
+        for(uint32_t i = 0; i<COLOR_MAX; i++)
+        {
+            uint16_t value1 = breathe_pwm_percent[breathe_seq][i];
+            uint16_t value2 = breathe_pwm_percent[breathe_seq+1][i];
+
+            if (value2 >= value1) 
+            {
+                diff[i] = value2 - value1;
+            } 
+            else 
+            {
+                diff[i] = -(int16_t)(value1 - value2);
+            }
+        }
     }//No else
 
     seq_count++;
@@ -82,22 +151,21 @@ void breathe_compute_set_seq_led_value(void)
     //Linear interpolation
     for(uint32_t i = 0; i<COLOR_MAX; i++)
     {
-        //interpolation = value1 + (value2 - value2)*(interval)
-        uint8_t value1 = breathe_pwm_value[breathe_seq][i];
-        uint8_t value2 = breathe_pwm_value[breathe_seq+1][i];
+        //interpolation = value1 + (value2 - value2)*(interval passed)
+        uint16_t interpolate = 
+                (breathe_pwm_percent[breathe_seq][i] + 
+                ((diff[i]*seq_count)/breathe_seq_len_count[breathe_seq]));
 
-        int16_t diff;
-        if (value2 >= value1) 
-        {
-            diff = value2 - value1;
-        } 
-        else 
-        {
-            diff = -(int16_t)(value1 - value2);
-        }
+        //tfp_printf("%d,",interpolate>>7);
 
-        colors[i] = (value1 + ((diff*seq_count)/breathe_seq_len_count[breathe_seq]));
+        interpolate = gamma2_correction_i16(interpolate);
+
+        //Max value of breathe_pwm_percent is 15 bit, making it 8 bit
+        colors[i] = interpolate >> 7;
+        //tfp_printf("%d,",colors[i]);
     }
+
+    //tfp_printf("\r\n");
 
     lp5810_set_manual_bgr_pwm(colors[BLUE], colors[GREEN], colors[RED]);
 }
@@ -107,6 +175,8 @@ void breathe_compute_set_seq_led_value(void)
 void breathe_periodic_handler(void)
 {
     static uint32_t timeout_count = BREATHE_MODE_TIMEOUT_COUNT;
+
+    breathe_compute_set_seq_led_value();
 
     //Handle button press to goto NIGHT mode
     bool btn_press = btn_handler_check_clear_press();
@@ -136,8 +206,6 @@ void breathe_periodic_handler(void)
             low_batt_mode_initiate();
         }//No else, continue in current mode
     }//No else
-
-    breathe_compute_set_seq_led_value();
 }
 
 void breathe_mode_initiate(void)
@@ -145,7 +213,7 @@ void breathe_mode_initiate(void)
     tfp_printf("BMODE\r\n");
 
     //Configure LP5810
-    lp5810_start_478_sequence();
+    lp5810_start_sequence();
 
     periodic_timer_set_second_callback(breathe_periodic_handler);
 }
